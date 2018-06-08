d7424cc22eeaaf8281736c3697c9e6d3743e4c95
riku-noname <31506547+riku-noname@users.noreply.github.com>
2018-06-08 14:17:08 +0900

    Update slack18.py

--- slack18.py
+++ slack18.py
@@ -12,5 +12,5 @@ if __name__ == '__main__':
         humid += float(data[1])
 
         #指定チャンネルへテキストの送信
-        slack = slackweb.Slack(url="https://hooks.slack.com/services/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")
+        slack = slackweb.Slack(url="https://hooks.slack.com/services/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
         slack.notify(text="あおらbotです．\n\n現在のKC104の温度，湿度に関して報告致します．\n\n-----\n時間 : 18時\n温度 : " + str(int(temp)) + " ℃\n湿度 : " + str(int(humid)) + "％\n-----\n\n以上です．")
